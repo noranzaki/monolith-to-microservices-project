117a4ba894bd2064e6538ef1522ac2726b83f929
noranzaki <noranzaki2012@gmail.com>
2023-02-23 21:06:05 +0200

    building frontend image

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a4985ab75d91c4de59399124038c16a0-1777035054.us-west-2.elb.amazonaws.com'
 };
 
 /*
